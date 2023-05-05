6af7d180f4c58e7b34f5371d19b2968849cb029e
Mahsa Bazzaz <mahsa.bazzaz@gmail.com>
2023-05-04 20:43:37 -0400

    Deploy

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,23 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+
+
+  <WelcomeItem>
+    
+    I'm a first-year PhD student at Khoury College of Computer Sciences.
+    I work in Game Design Lab under supervision of my great advisor Seth Cooper.
+    I graduted with BSc in Computer Engineering from AmirKabir University of Tehran.
+    I recently moved to Boston and started my PhD journey.
+    I love playing games and developing games! As if now, I'm doing research on PCGML in games.
+
+  </WelcomeItem>
+
+</template>
